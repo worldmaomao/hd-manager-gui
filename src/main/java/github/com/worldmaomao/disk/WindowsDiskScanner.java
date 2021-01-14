28e4cb24adef8a22633e1995fe05609e8ba48aee
Your Name <you@example.com>
2021-01-14 17:31:26 +0800

    feat:windows磁盘扫描

--- src/main/java/github/com/worldmaomao/disk/WindowsDiskScanner.java
+++ src/main/java/github/com/worldmaomao/disk/WindowsDiskScanner.java
@@ -2,6 +2,8 @@ package github.com.worldmaomao.disk;
 
 import github.com.worldmaomao.vo.LocalDiskVo;
 
+import javax.swing.filechooser.FileSystemView;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,8 +16,22 @@ public class WindowsDiskScanner implements DiskScanner {
 
     @Override
     public List<LocalDiskVo> scan() {
+        FileSystemView fsv = FileSystemView.getFileSystemView();
+        File[] roots = File.listRoots();
         List<LocalDiskVo> voList = new ArrayList<LocalDiskVo>();
+        for(File file : roots) {
+            LocalDiskVo diskVo = new LocalDiskVo();
+            diskVo.setName(fsv.getSystemDisplayName(file));
+            diskVo.setRootPath(file.getPath());
+            diskVo.setIsUsb(false);
+            voList.add(diskVo);
+        }
         return voList;
+    }
+
+    public static void main(String[] args) {
+        WindowsDiskScanner scanner = new WindowsDiskScanner();
+        System.out.println(scanner.scan());
     }
 
 }
